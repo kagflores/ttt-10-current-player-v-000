6159ed5cd8d0357106f6f8078667933066f874ea
Karen Flores <karen.ag.flores@gmail.com>
2018-03-04 09:46:30 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ def turn_count (board)
   counter = 1
   #counter = board.count( "O" ) + board.count( "X" )
   board.each do |turn|
-    if turn.downcase == "X" #|| turn.downcase == "O"
+    if turn == "O" #|| turn.downcase == "O"
     counter += 1
     puts "#{counter}"
   #elsif table == "O"
